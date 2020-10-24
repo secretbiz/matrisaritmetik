5ef81b4719a63421c60927b2132dc05b6f4aca83
semihM <smhmmc98@gmail.com>
2020-10-24 13:31:37 +0300

    Düzelt: MathJax hataları, Düzenle: Pages.Matris inline JS -> wwwroot\js

---
+++ MatrisAritmetik/wwwroot/js/mathjax_custom.js
@@ -0,0 +1,11 @@
+﻿function updateMathContent() {
+    try
+    {
+        var math = document.getElementById("matris_table");
+        MathJax.Hub.Queue(["Typeset", MathJax.Hub, math]);
+        //console.log("rerendering math...");
+    }
+    catch(err) {
+        console.log(err.message);
+    }
+} export { updateMathContent }
---
+++ MatrisAritmetik/wwwroot/js/matris_requests.js
@@ -0,0 +1,25 @@
+﻿import { updateMathContent } from './mathjax_custom.js';
+
+function addMatrix(event) {
+    var tkn = event.currentTarget.token;
+    $.ajax(
+        {
+            type: 'POST',
+            url: 'Matris?handler=AddMatrix',
+            data:
+            {
+                __RequestVerificationToken: tkn,
+                "name": document.getElementById("matris_name").value,
+                "vals": document.getElementById("matris_vals").value
+            },
+            success: function (data) { updateTable(tkn); },
+            error: function (error) { console.log(error); }
+        });
+}
+
+function updateTable(token) {
+    $('#matris_table').load('/Matris?handler=UpdateMatrisTable', { __RequestVerificationToken: token }, function () { updateMathContent();  })
+}
+
+const matris_add_button = document.getElementById("matris_add_button");
+if (matris_add_button) { matris_add_button.addEventListener("click", addMatrix, false); }
